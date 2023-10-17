b85d4e9ee880e4b393d7074d9897342dd91819d8
dialvare <dialvare@redhat.com>
2023-10-17 14:34:59 +0200

    javascript

--- javascript/app.py
+++ javascript/app.py
@@ -1,4 +1,18 @@
-from flask import Flask, render_template
+#!/bin/env python3
+import cv2
+import numpy as np
+from PIL import Image
+from flask import Flask, send_file, render_template, request, jsonify, Response
+from flask_socketio import SocketIO, emit
+import torch
+import threading
+import io
+from io import BytesIO
+import base64
+import time
+import os
+import eventlet
+import pandas
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
@@ -7,4 +21,4 @@ def render_index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
